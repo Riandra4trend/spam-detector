cbbdfe2bd3449ca73a8f3d2e61a62e9c6fbb5569
Riandra4trend <18221077@std.stei.itb.ac.id>
2025-06-21 15:39:47 +0700

    update preprocesses

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -4,11 +4,14 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+# Load tokenizer
+import pickle
+with open('tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
 
 MAX_LEN = 10000
-NUM_WORDS = 5000
-tokenizer = Tokenizer(num_words=NUM_WORDS)
+
+
 def clean_text(text: str) -> str:
     text = text.lower()
     text = re.sub(r"[^a-z0-9\s]", '', text)
